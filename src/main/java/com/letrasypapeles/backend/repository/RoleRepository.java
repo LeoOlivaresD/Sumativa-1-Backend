04841e26b7fd1fdc4f3747e8666dcb92c20e6d15
LeoOlivaresD <cspraword@gmail.com>
2025-06-06 23:36:23 -0400

    Refactor de role repository

--- src/main/java/com/letrasypapeles/backend/repository/RoleRepository.java
+++ src/main/java/com/letrasypapeles/backend/repository/RoleRepository.java
@@ -1,13 +1,10 @@
 package com.letrasypapeles.backend.repository;
 
-import com.letrasypapeles.backend.entity.Role;
+import com.letrasypapeles.backend.entity.RoleEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
-@Repository
-public interface RoleRepository extends JpaRepository<Role, String> {
-
-    Optional<Role> findByNombre(String nombre);
+public interface RoleRepository extends JpaRepository<RoleEntity, Long> {
+    Optional<RoleEntity> findByName(String name);
 }
