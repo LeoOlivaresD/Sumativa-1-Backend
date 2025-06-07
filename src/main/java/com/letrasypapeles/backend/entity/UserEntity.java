db25ac3d586c56ea0ee81383fe630c9dffb559ed
LeoOlivaresD <cspraword@gmail.com>
2025-06-06 23:30:44 -0400

    Commit inicial ( se cambia parte del pom, se refactoriza application properties y se implementan nuevas clases para preparar terreno con JWT

---
+++ src/main/java/com/letrasypapeles/backend/entity/UserEntity.java
@@ -0,0 +1,26 @@
+package com.letrasypapeles.backend.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "users")
+@Data
+@NoArgsConstructor
+public class UserEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+    private String password;
+
+    @ManyToMany(fetch = FetchType.EAGER, cascade = { CascadeType.PERSIST, CascadeType.MERGE })
+    @JoinTable(name = "user_roles", joinColumns = @JoinColumn(name = "user_id", referencedColumnName = "id"), inverseJoinColumns = @JoinColumn(name = "role_id", referencedColumnName = "id"))
+    private List<RoleEntity> roles = new ArrayList<>();
+
+}
