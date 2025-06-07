ca14ea54da90e21441f236d837117531173307bd
LeoOlivaresD <cspraword@gmail.com>
2025-06-06 23:33:19 -0400

    Se crea clase para manejar roles

---
+++ src/main/java/com/letrasypapeles/backend/entity/RoleEntity.java
@@ -0,0 +1,18 @@
+package com.letrasypapeles.backend.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+
+@Entity
+@Table(name = "roles")
+public class RoleEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+}
