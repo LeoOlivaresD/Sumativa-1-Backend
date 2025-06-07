7961e633aff93efa4ed2472f046e1bf5cd326b8f
LeoOlivaresD <cspraword@gmail.com>
2025-06-07 00:19:10 -0400

    Se crea clase con la configuracion de seguridad de la app

--- src/main/java/com/letrasypapeles/backend/security/SecurityConfig.java
+++ src/main/java/com/letrasypapeles/backend/security/SecurityConfig.java
@@ -1,3 +1,5 @@
+package com.letrasypapeles.backend.security;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -14,53 +16,67 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
+// Le indica al contenedor de spring que esta es una clase de seguridad al
+// momento de arrancar la aplicación
 @EnableWebSecurity
+// Indicamos que se activa la seguridad web en nuestra aplicación y además esta
+// será una clase la cual contendrá toda la configuración referente a la
+// seguridad
 public class SecurityConfig {
 
-	private JwtAuthEntryPoint authEntryPoint;
-	private final CustomUserDetailService userDetailsService;
+    private JwtAuthEntryPoint authEntryPoint;
+    private final CustomUserDetailService userDetailsService;
 
-	@Autowired
-	public SecurityConfig(CustomUserDetailService userDetailsService, JwtAuthEntryPoint authEntryPoint) {
-		this.userDetailsService = userDetailsService;
-		this.authEntryPoint = authEntryPoint;
-	}
+    @Autowired
+    public SecurityConfig(CustomUserDetailService userDetailsService, JwtAuthEntryPoint authEntryPoint) {
+        this.userDetailsService = userDetailsService;
+        this.authEntryPoint = authEntryPoint;
+    }
 
-	@Bean
-	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-		http
-				.csrf(csrf -> csrf.disable())
-				.exceptionHandling(exceptions -> exceptions.authenticationEntryPoint(authEntryPoint))
-				.sessionManagement(sessions -> sessions.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+    // Este bean va a encargarse de verificar la información de los usuarios que se
+    // loguearán en nuestra api
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration)
+            throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
 
-				.authorizeHttpRequests(authz -> authz
-						.requestMatchers("/api/auth/**").permitAll()
-						.requestMatchers("/empleado").hasRole("EMPLEADO")
-						.requestMatchers("/gerente").hasRole("GERENTE")
-						.requestMatchers("/cliente").hasRole("CLIENTE")
-						.anyRequest().authenticated()
+    // Con este bean nos encargaremos de encriptar todas nuestras contraseñas
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
-				);
-		http.addFilterBefore(jwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
+    // Este bean incorporará el filtro de seguridad de json web token que creamos en
+    // nuestra clase anterior
+    @Bean
+    public JwtAuthenticationFilter jwtAuthenticationFilter() {
+        return new JwtAuthenticationFilter();
+    }
 
-		return http.build();
+    // Vamos a crear un bean el cual va a establecer una cadena de filtros de
+    // seguridad en nuestra aplicación.
+    // Y es aquí donde determinaremos los permisos segun los roles de usuarios para
+    // acceder a nuestra aplicación
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable())
+                .exceptionHandling(exceptions -> exceptions.authenticationEntryPoint(authEntryPoint))
+                .sessionManagement(sessions -> sessions.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
 
-	}
+                .authorizeHttpRequests(authz -> authz
+                        .requestMatchers("/api/auth/**").permitAll()
+                        .requestMatchers("/empleado").hasRole("EMPLEADO")
+                        .requestMatchers("/gerente").hasRole("GERENTE")
+                        .requestMatchers("/cliente").hasRole("CLIENTE")
+                        .anyRequest().authenticated()
 
-	@Bean
-	public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration)
-			throws Exception {
-		return authenticationConfiguration.getAuthenticationManager();
-	}
+                );
+        http.addFilterBefore(jwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
 
-	@Bean
-	PasswordEncoder passwordEncoder() {
-		return new BCryptPasswordEncoder();
-	}
+        return http.build();
 
-	@Bean
-	public JwtAuthenticationFilter jwtAuthenticationFilter() {
-		return new JwtAuthenticationFilter();
-	}
+    }
 
 }
