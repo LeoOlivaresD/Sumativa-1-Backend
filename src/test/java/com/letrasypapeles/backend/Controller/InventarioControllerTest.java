6482ac442f4975595e1043b61e7e3c2120d1d34d
Jaime Barraza <jai.barraza@duocuc.cl>
2025-06-30 21:37:46 -0400

    ClienteControllerTest ok

--- src/test/java/com/letrasypapeles/backend/Controller/InventarioControllerTest.java
+++ src/test/java/com/letrasypapeles/backend/Controller/InventarioControllerTest.java
@@ -1,23 +1,30 @@
 package com.letrasypapeles.backend.controller;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.letrasypapeles.backend.config.TestSecurityConfig;
-import com.letrasypapeles.backend.entity.Inventario;
-import com.letrasypapeles.backend.service.InventarioService;
+import java.util.List;
+import java.util.Optional;
+
 import org.junit.jupiter.api.Test;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import java.util.*;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.letrasypapeles.backend.config.TestSecurityConfig;
+import com.letrasypapeles.backend.entity.Inventario;
+import com.letrasypapeles.backend.service.InventarioService;
 
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
 @Import(TestSecurityConfig.class)
 @WebMvcTest(InventarioController.class)
 class InventarioControllerTest {
